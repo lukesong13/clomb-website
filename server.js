8c754c84d52bfeb1f2145d5632fe9ce57779cacf
lukesong13 <lsongsr20@gmail.com>
2025-05-25 12:54:46 -0700

    added nav bar, bootstrap, updated server.js to use pub directory

--- server.js
+++ server.js
@@ -1,46 +1,34 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Serve static files from the directory containing index.html
-// app.use(express.static(path.join(__dirname)));
-
-// app.listen(port, () => {
-//     console.log(`Server started. Available at http://localhost:${port}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files (JS, CSS, images, etc.)
-app.use(express.static(path.join(__dirname)));
+// Serve static files from /public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for root
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Route for /users
 app.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/users.html'));
+    res.sendFile(path.join(__dirname, 'public', 'users.html'));
 });
 
 // Route for /gyms
 app.get('/gyms', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/gyms.html'));
+    res.sendFile(path.join(__dirname, 'public', 'gyms.html'));
 });
 
 // Route for /routes
 app.get('/routes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/routes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'routes.html'));
 });
 
 // Route for /climbdetails
 app.get('/climbdetails', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/climbdetails.html'));
+    res.sendFile(path.join(__dirname, 'public', 'climb-details.html'));
 });
 
 app.listen(port, () => {
